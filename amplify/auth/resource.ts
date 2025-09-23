82e188f2d06a2a794985492e4caa5e9daea4bc88
Arham Riaz <riaz@bridge-wireless.com>
2025-09-23 10:23:43 -0400

    working auth/resource.ts file, correct cognito pool

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,12 +1,21 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, referenceAuth } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ * @see c
  */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
+export const auth = referenceAuth({
+  userPoolId: 'ca-central-1_gqbunB8W8',
+  identityPoolId: 'ca-central-1:1591fd5e-28fd-4a40-9bc6-fe092b6ef4e0',
+  authRoleArn:
+    'arn:aws:iam::216348571084:role/amplify-MyTrainingApp-fay-amplifyAuthauthenticatedU-hPaEMQjurgGi',
+  unauthRoleArn:
+    'arn:aws:iam::216348571084:role/amplify-MyTrainingApp-fay-amplifyAuthunauthenticate-FczBMKKQDLm4',
+  userPoolClientId: '44vfska095jcbkchft96d0ula0',
+  groups: {
+    Employees:
+      'arn:aws:iam::216348571084:role/amplify-MyTrainingApp-fay-amplifyAuthEmployeesGroup-emCoB9LQlkSx',
+    Managers:
+      'arn:aws:iam::216348571084:role/amplify-MyTrainingApp-fay-amplifyAuthManagersGroupR-vGeGEbb7nvi6',
   },
-  groups: ['Employees', 'Managers'] // Define user groups for roles
 });
