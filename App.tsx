d252f5083a26f8a47a07a11fa190c29fe8374419
circular360dev <circular360dev@gmail.com>
2025-11-24 11:55:32 +0530

    Remove space second push test

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ import outputs from './amplify_outputs.json';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import { CourseList } from './CourseList';
 
-
 // This line configures Amplify with the backend info from your sandbox
 Amplify.configure(outputs);
 
